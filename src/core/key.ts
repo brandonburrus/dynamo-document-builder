449f59ad7deb38c451c60423ac27dfca1f70481d
Brandon Burrus <brandon@burrus.io>
2026-01-14 18:39:28 -0700

    feat: support sparse indexes

--- src/core/key.ts
+++ src/core/key.ts
@@ -1,25 +1,76 @@
+import { DocumentBuilderError } from '@/errors'
 import type { IndexName } from './core-types'
 
 export type DynamoKeyableValue = string | number | Buffer
+export type DynamoIndexKeyableValue = string | number | Buffer | undefined
+
 export type DynamoKeyBuilder<Item> = (item: Item) => DynamoKeyableValue
+export type DynamoIndexKeyBuilder<Item> = (item: Item) => DynamoIndexKeyableValue
 
 export type GlobalSecondaryIndexKeyBuilders<Item> = Record<
   IndexName,
   {
-    partitionKey: DynamoKeyBuilder<Item>
-    sortKey?: DynamoKeyBuilder<Item>
+    partitionKey: DynamoIndexKeyBuilder<Item>
+    sortKey?: DynamoIndexKeyBuilder<Item>
   }
 >
 
 export type LocalSecondaryIndexKeyBuilders<Item> = Record<
   IndexName,
   {
-    sortKey: DynamoKeyBuilder<Item>
+    sortKey: DynamoIndexKeyBuilder<Item>
   }
 >
 
 export type DynamoKey = Record<string, DynamoKeyableValue>
+export type DynamoIndexKey = Record<string, DynamoIndexKeyableValue>
 
+/**
+ * Builds a DynamoDB key from the given parts by concatenating them with a '#' separator.
+ * At least one part must be provided, or a DocumentBuilderError will be thrown.
+ *
+ * @param parts - The parts to combine into a key.
+ * @returns The combined key as a string.
+ *
+ * @example
+ * ```ts
+ * const key1 = key('USER', 123) // 'USER#123'
+ * const key2 = key('METADATA') // 'METADATA'
+ * ```
+ */
 export function key(...parts: DynamoKeyableValue[]): string {
+  if (!parts.length) {
+    throw new DocumentBuilderError('At least one key part must be provided')
+  }
   return parts.join('#')
+}
+
+/**
+ * Builds a DynamoDB key from the given parts. This function is specifically for secondary index keys,
+ * where any part can be undefined. If any part is undefined, the function returns undefined.
+ *
+ * For primary keys, use the `key` function instead.
+ *
+ * This is specifically useful for building keys for queries on secondary indexes, or for sparse indexes.
+ *
+ * @param parts - The parts to combine into a key.
+ * @returns The combined key as a string, or undefined if any part is undefined.
+ *
+ * @example
+ * ```ts
+ * const key1 = indexKey('part1', 'part2', 'part3') // 'part1#part2#part3'
+ * const key2 = indexKey('part1', undefined, 'part3') // undefined
+ * const key3 = indexKey(undefined, 'part2', 'part3') // undefined
+ * const key4 = indexKey('part1', 'part2', undefined) // undefined
+ * ```
+ */
+export function indexKey(...parts: DynamoIndexKeyableValue[]): string | undefined {
+  let key: string = ''
+  for (const part of parts) {
+    if (part === undefined) {
+      return undefined
+    }
+    key += key ? `#${part}` : part
+  }
+  return key
 }
--- tests/unit/core/key.test.ts
+++ tests/unit/core/key.test.ts
@@ -1,4 +1,4 @@
-import { key } from '@/core'
+import { key, indexKey } from '@/core'
 import { describe, it, expect } from 'vitest'
 
 describe('key', () => {
@@ -7,5 +7,25 @@ describe('key', () => {
     expect(key('order', '456')).toBe('order#456')
     expect(key('product', 789)).toBe('product#789')
     expect(key('USER', 123, 'TYPE', 'ADMIN')).toBe('USER#123#TYPE#ADMIN')
+  })
+
+  it('should throw an error if no parts are provided', () => {
+    expect(() => key()).toThrowError('At least one key part must be provided')
+  })
+})
+
+describe('indexKey', () => {
+  it('should create dynamo index key strings', () => {
+    expect(indexKey('user', '123')).toBe('user#123')
+    expect(indexKey('order', '456')).toBe('order#456')
+    expect(indexKey('product', 789)).toBe('product#789')
+    expect(indexKey('USER', 123, 'TYPE', 'ADMIN')).toBe('USER#123#TYPE#ADMIN')
+  })
+
+  it('should return undefined if any part is undefined', () => {
+    expect(indexKey('user', undefined)).toBeUndefined()
+    expect(indexKey(undefined, '123')).toBeUndefined()
+    expect(indexKey('order', '456', undefined)).toBeUndefined()
+    expect(indexKey(undefined)).toBeUndefined()
   })
 })
